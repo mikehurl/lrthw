3282388d2d222d3bd568f949ac149f62d825fc83
Mike Hurl <mikehurl@gmail.com>
2016-10-17 13:58:59 +0000

    exercise 3 code

---
+++ chap03/ex3.rb
@@ -0,0 +1,24 @@
+# This will print text
+puts "I will now count my chickens:"
+# These next two lines will print the initial word and the result of the sum within the brackets
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+# This will print text
+puts "Now I will count the eggs:"
+# This will print the result of the sum
+puts 3 + 2 + 1 - 5 + 4  % 2 - 1 / 4 + 6
+# This will print a string
+puts "Is it true that 3 + 2 < 5 - 7?"
+# This code would print 'true' or 'false' depending on the result of the sum ('false' in this case)
+puts 3 + 2 < 5 - 7
+# This will put two lines asking a question and the result of the sum in the brackets
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+# This will print the text
+puts "Oh, that's why it's false."
+# This will print the text
+puts "How about some more?"
+# These three lines will print the initial text and then either 'true' or 'false' depending on the result of greater than / less than / greater than or equal to etc
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
