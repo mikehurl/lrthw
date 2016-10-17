e542f9f69a20bb4fcfd248eb0dfbd661738c15ff
Mike Hurl <mikehurl@gmail.com>
2016-10-17 14:58:38 +0000

    exercise 5 code created

---
+++ chap05/ex5.rb
@@ -0,0 +1,21 @@
+name = 'Mike Hurl'
+age = 36 # not a lie in 2016
+height = 81 # inches
+weight = 154 # pounds
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_in_cms = height * 2.54
+weight_in_kgs = weight * 0.45
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height} and #{weight} I get #{age + height + weight}."
+
+puts "When converted from inches, he's #{height_in_cms} centimetres tall."
+puts "When converted from pounds, he's #{weight_in_kgs} kilograms heavy."
